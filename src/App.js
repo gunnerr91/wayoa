04caa2b20c8685083ddd91c459cf56fef096e686
gunnerr91 <blissvortex@hotmail.com>
2020-07-07 17:50:04 +0100

    finished grid setup for the item layout

--- src/App.js
+++ src/App.js
@@ -4,52 +4,38 @@ import ghostImage from "./assets/images/ghost.jpg";
 import cyberImage from "./assets/images/cyberpunk.webp";
 import mafiaImage from "./assets/images/mafia.jpg";
 
-const imageStyle = {
-  verticalalign: "middle",
-  width: "80px",
-  height: "80px",
-  borderRadius: "50%",
-};
-
-const divStyle = {
-  backgroundColor: "white",
-  color: "black",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-};
-
 function App() {
   let games = [
     {
       name: "Ghost of Tsushima",
       releaseDate: new Date(2020, 7, 17),
-      imageSrc: ghostImage,
+      imageSrc: ghostImage
     },
     {
       name: "Cyberpunk 2077",
       releaseDate: new Date(2020, 11, 19),
-      imageSrc: cyberImage,
+      imageSrc: cyberImage
     },
     {
       name: "Mafia: Definitive Edition",
       releaseDate: new Date(2020, 8, 28),
-      imageSrc: mafiaImage,
-    },
+      imageSrc: mafiaImage
+    }
   ];
   games.sort((a, b) => a.releaseDate - b.releaseDate);
 
   return (
-    <section class="mainApp">
+    <div className="mainApp">
       {games.map((game, index) => (
-        <div style={divStyle} key={index}>
-          <img style={imageStyle} src={game.imageSrc} />
-          <p>{game.name}</p>
-          <p>{game.releaseDate.toUTCString()}</p>
+        <div className="commonDivStyle" key={index}>
+          <img className="imageStyle" src={game.imageSrc} />
+          <div className="textStyle">
+            <p>{game.name}</p>
+            <p>{game.releaseDate.toUTCString()}</p>
+          </div>
         </div>
       ))}
-    </section>
+    </div>
   );
 }
 
